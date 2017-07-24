0b7940e30f6ce6cd2c0255cf946ba56a9a797cc5
esiu06 <pawel.esmanowicz@wp.pl>
2017-07-24 20:38:14 +0200

    comit2

--- ALGORYTMY/src/Algorytmy/Wypłata.java
+++ ALGORYTMY/src/Algorytmy/Wypłata.java
@@ -13,8 +13,8 @@ public class Wypłata extends AbstractAlgoritm {
     public void runAlgoritm(String[] input) {
 
 
-       int[] zl  = {200, 100, 50, 20,10, 5, 2, 1};
-        int[] gr = {50,20,10, 5, 2, 1};
+        int[] zl = {200, 100, 50, 20, 10, 5, 2, 1};
+        int[] gr = {50, 20, 10, 5, 2, 1};
 
         int wyplata1 = Integer.parseInt(input[1]);
         int wyplata2 = Integer.parseInt(input[2]);
@@ -22,24 +22,25 @@ public class Wypłata extends AbstractAlgoritm {
         int[] wyplatazl = new int[8];
         int[] wyplatagr = new int[6];
 
-        while( wyplata1>0) {
-
+        while (wyplata1 > 0) {
             for (int i = 0; i < zl.length; i++) {
-                if (wyplatazl[i] >= zl[i]) ;
-                wyplatazl[i] = wyplatazl[i] - zl[i];
-                wyplatazl[i]++;
-                break;
+                if (wyplata1 >= zl[i]) {
+                    wyplata1 = wyplata1 - zl[i];
+                    wyplatazl[i]++;
+                    break;
+                }
             }
         }
-        while (wyplata2>0){
-            for( int j=0; j< gr.length; j++){
-                if( wyplatagr[j] >= gr[j]);
-                    wyplatagr[j] = wyplatagr[j] - gr[j];
-                wyplata2++;
-                break;
+        while (wyplata2 > 0) {
+            for (int j = 0; j < gr.length; j++) {
+                if (wyplata2 >= gr[j]) {
+                    wyplata2 = wyplata2 - gr[j];
+                    wyplata2++;
+                    break;
+                }
+
             }
 
         }
-
     }
 }
