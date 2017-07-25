559f0efd76c81ccfd277664fa3b71e57ad856e48
esiu06 <pawel.esmanowicz@wp.pl>
2017-07-25 19:07:30 +0200

    silnia fibonacci

---
+++ ALGORYTMY/src/Algorytmy/CiagFibonacciego.java
@@ -0,0 +1,34 @@
+package Algorytmy;
+
+/**
+ * Created by RENT on 2017-07-25.
+ */
+public class CiagFibonacciego extends AbstractAlgoritm {
+    @Override
+    public String getName() {
+        return "Ciag Fibonacciego";
+    }
+
+    @Override
+    public String runAlgoritm(String[] input) {
+
+        int n = Integer.parseInt(input[1]);
+
+        System.out.println("Ciag Fibunacciego %d wynosi %d: ", n, Fiboncci(n));
+    }
+
+
+    private int Fiboncci(int n){
+        if (n==0){
+            return 0;
+        }
+        else if (n==1){
+            return 1;
+        }
+        else {
+            return Fiboncci(n-1)+ Fiboncci(n-2);
+
+        }
+
+    }
+}
---
+++ ALGORYTMY/src/Algorytmy/Silnia3.java
@@ -0,0 +1,52 @@
+package Algorytmy;
+
+/**
+ * Created by RENT on 2017-07-25.
+ */
+public class Silnia3 extends AbstractAlgoritm {
+    @Override
+    public String getName() {
+        return "Liczenie silni 3";
+    }
+
+    @Override
+    public String runAlgoritm(String[] input) {
+
+        int n = Integer.parseInt(input[1]);
+
+        switch (n){
+            case 1:
+                System.out.println("0 1");
+                break;
+            case 2:
+                System.out.println("0 2");
+                break;
+            case 3:
+                System.out.println("0 6");
+                break;
+            case 4:
+                System.out.println("2 4");
+                break;
+            case 5:
+                System.out.println("2 0");
+                break;
+            case 6:
+                System.out.println("2 0");
+                break;
+            case 7:
+                System.out.println("4 0 ");
+                break;
+            case 8:
+                System.out.println("2 0");
+                break;
+            case 9:
+                System.out.println("8 0");
+                break;
+
+            default:
+                return "0 0";
+
+        }
+        return null;
+    }
+}
